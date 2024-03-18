b54959043775cb077b2237ecb7e1e446f2085a9b
Thomas <thomas.coombes23@gmail.com>
2024-03-18 20:24:53 +0000

    add spider.py and implement spider class

---
+++ spider.py
@@ -0,0 +1,19 @@
+class Spider:
+    def __init__(self, target=None, scan_depth=1, username=None, password=None):
+        self.target = target
+        self.scan_depth = scan_depth
+        self.username = username
+        self.password = password
+        
+            
+    def start_spider(self):
+        print("\nSpider started on target:", self.target)
+		
+	
+	
+	
+	
+		
+if __name__ == "__main__":
+        main()
+			
